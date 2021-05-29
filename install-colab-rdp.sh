b1d2cec0b31d8cb251cf93af834970f4b0d74e72
Mitheshjn <72849423+Mitheshjn@users.noreply.github.com>
2021-05-29 19:09:35 +0530

    Update install-colab-rdp.sh

--- install-colab-rdp.sh
+++ install-colab-rdp.sh
@@ -4,4 +4,8 @@ wget https://excellmedia.dl.sourceforge.net/project/flavia/Leaf%20Image%20Datase
 wget https://excellmedia.dl.sourceforge.net/project/flavia/Standard%20Leaf%20Images/0.1/standardleaves.tar.bz2 
 tar -xjvf Leaves.tar.bz2 
 tar -xjvf standardleaves.tar.bz2 
+sudo add-apt-repository ppa:notepadqq-team/notepadqq
+sudo apt-get update
+sudo apt-get install notepadqq
+
 #etc.
