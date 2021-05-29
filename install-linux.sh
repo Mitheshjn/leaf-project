1fa2c4ae666f55547c7e49b1339002bb419f3006
Mitheshjn <72849423+Mitheshjn@users.noreply.github.com>
2021-05-29 18:35:10 +0530

    Update install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -1,17 +1,10 @@
 #!/bin/sh 
 
-wget https://excellmedia.dl.sourceforge.net/project/flavia/Leaf%20Image%20Dataset/1.0/Leaves.tar.bz2 -y
-
-wget https://excellmedia.dl.sourceforge.net/project/flavia/Standard%20Leaf%20Images/0.1/standardleaves.tar.bz2 -y
-
-tar -xjvf Leaves.tar.bz2 -y
-
-tar -xjvf standardleaves.tar.bz2 -y
-
-pip3 install mahotas -y
-
-pip3 install Django -y
-
-git clone https://github.com/Mitheshjn/leaf-project.git -y
+wget https://excellmedia.dl.sourceforge.net/project/flavia/Leaf%20Image%20Dataset/1.0/Leaves.tar.bz2 
+wget https://excellmedia.dl.sourceforge.net/project/flavia/Standard%20Leaf%20Images/0.1/standardleaves.tar.bz2 
+tar -xjvf Leaves.tar.bz2 
+tar -xjvf standardleaves.tar.bz2 
+pip3 install mahotas 
+pip3 install Django 
 
 #etc.
