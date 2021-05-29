1b768ec72d549d3fd32157fca4e42c06f32c1f8e
Mitheshjn <72849423+Mitheshjn@users.noreply.github.com>
2021-05-29 18:34:32 +0530

    Update install-colab-rdp.sh

--- install-colab-rdp.sh
+++ install-colab-rdp.sh
@@ -1,13 +1,7 @@
 #!/bin/sh 
 
-wget https://excellmedia.dl.sourceforge.net/project/flavia/Leaf%20Image%20Dataset/1.0/Leaves.tar.bz2 -y
-
-wget https://excellmedia.dl.sourceforge.net/project/flavia/Standard%20Leaf%20Images/0.1/standardleaves.tar.bz2 -y
-
-tar -xjvf Leaves.tar.bz2 -y
-
-tar -xjvf standardleaves.tar.bz2 -y
-
-git clone https://github.com/Mitheshjn/leaf-project.git -y
-
+wget https://excellmedia.dl.sourceforge.net/project/flavia/Leaf%20Image%20Dataset/1.0/Leaves.tar.bz2 
+wget https://excellmedia.dl.sourceforge.net/project/flavia/Standard%20Leaf%20Images/0.1/standardleaves.tar.bz2 
+tar -xjvf Leaves.tar.bz2 
+tar -xjvf standardleaves.tar.bz2 
 #etc.
